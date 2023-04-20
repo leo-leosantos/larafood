59fd913a62ff091c244b3efbc654f82d04cf0734
Informatica ESPMG <informatica@esp.mg.gov.br>
2023-04-20 16:47:25 -0300

    03 - Gestão de Detalhes do Plano - Aula 01  Organizar as Rotas de Admin no Larafood

---
+++ app/Models/Plan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = [
+        'name','url','price', 'description'
+    ];
+
+    public function search($filter = null)
+    {
+        $results = $this
+                    ->where('name','LIKE', "%{$filter}%")
+                    ->where('description','LIKE', "%{$filter}%")
+                    ->paginate();
+
+        return $results;
+    }
+}

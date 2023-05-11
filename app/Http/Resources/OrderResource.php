9312d651df39e5e6f183cba806185c20abeb2011
TIC ESPMG <leandro.santos@esp.mg.gov.br>
2023-05-11 15:06:42 -0300

    23 - Melhorias e Correções  05 - Melhorar Resources API Laravel Api

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
@@ -21,7 +22,9 @@ class OrderResource extends JsonResource
             'client'=>$this->client_id  ? new ClientResource($this->client) : '',
             'table'=>$this->table_id ? new TableResource($this->table) : '',
             'products'=> ProductResource::collection($this->products) ,
-
+            'date'=> Carbon::make($this->created_at)->format('Y-m-d H:i:s'),
+            'Company'=> New TenantResource($this->tenant),
+            'evaluations'=> EvaluationResource::collection($this->evaluations) ,
         ];
     }
 }

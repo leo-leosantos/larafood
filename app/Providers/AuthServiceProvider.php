1fbed5a88bbcd47f2c58c811255f411068bd795b
TIC ESPMG <leandro.santos@esp.mg.gov.br>
2023-05-11 13:39:15 -0300

    23 - Melhorias e Correções - 01 - Merge branch API com master

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,6 +29,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        if($this->app->runningInConsole()){
+            return;
+        }
         $permissions = Permission::all();
 
         foreach($permissions as $permission)
